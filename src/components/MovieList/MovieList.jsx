295e77b325d2dd45c40f0e5e43d996d6e6d42596
Oleg Zhdanenko <Olegzhdan1988@gmail.com>
2024-03-26 14:59:27 +0300

    update movielist

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,15 +1,18 @@
-import {  useLocation } from "react-router-dom"
-import MovieElement from "../MovieElement/MovieElement"
-import css from "../MovieList/MovieList.module.css"
+import { useLocation } from "react-router-dom";
+import MovieElement from "../MovieElement/MovieElement";
+import css from "../MovieList/MovieList.module.css";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+
+
 export default function MovieList({ movies }) {
     const location = useLocation()
-    console.log(location);
     return (
         <ul className={css.list}>
             {movies.map((movie) => {
-                return <MovieElement key={movie.id} movie={movie} state={location} />   
+                
+                return movies.length > 0 ? <MovieElement key={movie.id} movie={movie} state={location} /> : <ErrorMessage />
             })
             }
-            </ul>
-    )
+        </ul>
+    );
 }
